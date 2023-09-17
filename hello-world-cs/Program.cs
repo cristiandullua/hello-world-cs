0c0b5013a8c5d1b6693d150072ad17be5843fec9
Cristian D Ullua <cris.ullua.95@gmail.com>
2023-09-17 01:01:52 -0300

    add everything

---
+++ hello-world-cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Wait for a key press before exiting
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
